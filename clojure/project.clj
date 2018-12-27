f5130bf2f91bc1bae0b53e639193dc5211312d9e
Anthony Khong <anthony.kusumo.khong@gmail.com>
2018-12-27 07:10:06 +0700

    Added REBL

--- clojure/project.clj
+++ clojure/project.clj
@@ -4,5 +4,8 @@
   :url "http://github.com/technomancy/myproject"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [incanter "1.9.3"]
-                 [metasoarous/oz "1.4.1"]] 
-  :plugins [[lein-tar "3.2.0"]]) 
+                 [metasoarous/oz "1.4.1"]
+                 [com.bhauman/rebel-readline "0.1.4"]] 
+  :plugins [[lein-tar "3.2.0"]]
+  :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]})
+   
