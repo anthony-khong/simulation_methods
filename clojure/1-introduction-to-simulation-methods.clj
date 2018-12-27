b4e80b11d4ce8b2e7a684a5a988492c8973ff69b
Anthony Khong <anthony.kusumo.khong@gmail.com>
2018-12-27 15:41:49 +0700

    Finished q2

--- clojure/1-introduction-to-simulation-methods.clj
+++ clojure/1-introduction-to-simulation-methods.clj
@@ -1,7 +1,6 @@
 (use '(incanter core stats charts io))
 
-(def xs (sample-normal 5000))
-
+; Utility Functions
 (defn standardise [xs]
   (let [mu (mean xs)
         sigma (sd xs)]
@@ -17,6 +16,7 @@
     {:std-quantiles (quantile-map std-xs [0.025, 0.15, 0.5, 0.85, 0.975])
      :mean (mean xs)
      :var (variance xs)
+     :sd (sd xs)
      :kurtosis (kurtosis xs)
      :skewness (skewness xs)}))
 
@@ -27,7 +27,7 @@
 (defn sample-averages [n-sims n-rows]
   (take n-sims (repeatedly #(sample-single-average n-rows))))
 
-(for [i (range 1 15 3)
+(for [i (range 1 14 4)
       :let [n-rows (int (Math/pow 2 i))]]
   (-> (sample-averages 5000 n-rows)
       (summarise)
@@ -38,12 +38,39 @@
 (defn expectation-of-average [n-rows] 0.5)
 (defn variance-of-average [n-rows] (/ 1/12 n-rows))
 
-(defn summarise [n-rows] {:n-rows n-rows 
-                          :mean (expectation-of-average n-rows)
-                          :var ( variance-of-average n-rows)})
+(defn expectations [n-rows] {:n-rows n-rows
+                             :mean (expectation-of-average n-rows)
+                             :var (variance-of-average n-rows)})
 
-(pprint
-  (let [n-rows-grid (map #(Math/pow 2 %) (range 1 10))]
-    (map summarise n-rows-grid)))
-   
+(for [i (range 1 15 3)
+      :let [n-rows (int (Math/pow 2 i))]]
+  (pprint (expectations n-rows)))
+
 ; Question 2
+(defn sample-sigle-delta [n-x n-y]
+  (let [mean-x (mean (sample-exp n-x))
+        mean-y (mean (sample-gamma n-y :shape 3 :scale 2))]
+    (- mean-x mean-y)))
+
+(defn sample-deltas [n-sims & {:keys [n-x n-y]}]
+  (take n-sims (repeatedly #(sample-sigle-delta n-x n-y))))
+
+(for [i (range 1 14 4)
+      :let [n-rows (int (Math/pow 2 i))]]
+  (-> (sample-deltas 5000 :n-x n-rows :n-y n-rows)
+      (summarise)
+      (assoc :n-rows n-rows)
+      (pprint)))
+
+(defn expectation-of-delta [n-x n-y]  
+  {:mean (- 1 (* 3 2))
+   :std-error (let [var-x (/ 1 n-x)
+                    var-y (/ (* 2 (* 3 3)) n-y)]
+                (Math/sqrt (+ var-x var-y)))})
+(expectation-of-delta 100 100)
+
+(for [i (range 1 14 4)
+      :let [n-rows (int (Math/pow 2 i))]]
+  (-> (expectation-of-delta n-rows n-rows)
+      (assoc :n-rows n-rows)
+      (pprint)))
--- clojure/project.clj
+++ clojure/project.clj
@@ -4,6 +4,7 @@
   :url "http://github.com/technomancy/myproject"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [incanter "1.9.3"]
+                 [cider/cider-nrepl "0.18.0"]
                  [metasoarous/oz "1.4.1"]
                  [com.bhauman/rebel-readline "0.1.4"]] 
   :plugins [[lein-tar "3.2.0"]]
