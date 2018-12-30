f97957afe4c112e824fb5aa2de67457300ba1950
akkhong <anthony.kusumo.khong@gmail.com>
2018-12-30 17:32:44 +0700

    .

--- fsharp/1-introduction-to-simulation-methods.fsx
+++ fsharp/1-introduction-to-simulation-methods.fsx
@@ -4,12 +4,24 @@ open MathNet.Numerics.LinearAlgebra
 
 open MathNet.Numerics.Statistics
 
-let xs = DenseVector.randomStandard<float> 10
-
 Statistics.Mean(xs)
 
 Statistics.Variance(xs)
 
 Statistics.percentileFunc xs 99
 
+#load "Stats.fsx"
 
+let mean (xs: float seq) = Statistics.Mean(xs)
+let variance (xs: float seq) = Statistics.Variance(xs)
+
+let xs = DenseVector.randomStandard<float> 10
+
+
+type Shape = int | int * int
+
+let rnorm shape = 
+    match shape with
+    | (n1, n2) -> DenseMatrix.randomStandard<float> n1 n2
+    | n -> DenseMatrix.randomStandard<float> n 1
+    | _ -> None
