8ea0e8cb181531659bfbcaadbad0a9eed11f2732
Anthony Khong <anthony.kusumo.khong@gmail.com>
2018-12-31 13:44:48 +0700

    Added support for both continuous and discrete distributions

--- fsharp/1-introduction-to-simulation-methods.fsx
+++ fsharp/1-introduction-to-simulation-methods.fsx
@@ -1,27 +1,5 @@
-#load "packages/FsLab/FsLab.fsx"
-
-open MathNet.Numerics.LinearAlgebra
-
-open MathNet.Numerics.Statistics
-
-Statistics.Mean(xs)
-
-Statistics.Variance(xs)
-
-Statistics.percentileFunc xs 99
-
 #load "Stats.fsx"
 
-let mean (xs: float seq) = Statistics.Mean(xs)
-let variance (xs: float seq) = Statistics.Variance(xs)
+open Stats
 
-let xs = DenseVector.randomStandard<float> 10
-
-
-type Shape = int | int * int
-
-let rnorm shape = 
-    match shape with
-    | (n1, n2) -> DenseMatrix.randomStandard<float> n1 n2
-    | n -> DenseMatrix.randomStandard<float> n 1
-    | _ -> None
+describe (rnorm 10.0 0.1 1000)
--- fsharp/Stats.fsx
+++ fsharp/Stats.fsx
@@ -4,38 +4,69 @@ open MathNet.Numerics.Distributions
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.Statistics
 
+// Descriptive Statistics
 type Quantiles = 
     {Min:float; Q1:float; Median:float; Q3:float; Max:float}
 
 type Description = {Mean:float; Std:float; Qs:Quantiles}
 
 let mean (xs: float seq) = Statistics.Mean(xs)
-
 let std (xs: float seq) = Statistics.StandardDeviation(xs)
-
 let variance (xs: float seq) = Statistics.Variance(xs)
-
 let min = Seq.min
-
 let max = Seq.max
-
 let quantiles (xs: float seq) =
     let quantile = fun (quantile) -> Statistics.Percentile(xs, quantile)
     {Min=min xs; Q1=quantile 25; Median=quantile 50; Q3=quantile 75; Max=max xs}
-
 let describe (xs: float seq) = {Mean=mean xs; Std=std xs; Qs=quantiles xs}
 
-let rdist distribution nSamples = DenseVector.random<float> nSamples distribution
+// Common Distribution Functions
+type Dist =
+    | Continuous of IContinuousDistribution
+    | Discrete of IDiscreteDistribution
 
-let rnorm mean std = rdist (Normal(mean, std))
+let rdist distribution nSamples = 
+    match distribution with
+    | (Continuous dist) -> DenseVector.random<float> nSamples dist
+    | (Discrete dist) -> 
+        dist.Samples()
+        |> Seq.map float
+        |> Seq.take nSamples
+        |> DenseVector.ofSeq
 
-let rexp rate = rdist (Exponential(rate))
+let qdist distribution = 
+    match distribution with
+    | (Continuous dist) -> Vector.map dist.CumulativeDistribution
+    | (Discrete dist) -> Vector.map dist.CumulativeDistribution
 
-let rgamma shape rate = rdist (Gamma(shape, rate))
+// Binomial Distribution
+let binom prob nTrials = Discrete (Binomial(prob, nTrials))
+let rbinom prob nTrials = binom prob nTrials |> rdist
+let qbinom prob nTrials = binom prob nTrials |> qdist
 
-let rcauchy location scale = rdist (Cauchy(location, scale))
+// Normal Distribution
+let norm mean std = Continuous (Normal(mean, std))
+let rnorm mean std = norm mean std |> rdist
+let qnorm mean std = norm mean std |> qdist
 
-let rstdNorm = rdist (Normal(0.0, 1.0))
+let stdNorm = norm 0.0 1.0
+let rstdNorm = rdist stdNorm
+let qstdNorm xs = qdist stdNorm xs
+
+// Exponential Distribution
+let exponential rate = Continuous (Exponential(rate))
+let rexp rate = exponential rate |> rdist
+let qexp rate = exponential rate |> qdist
+
+// Gamma Distribution
+let gamma shape rate = Continuous (Gamma(shape, rate))
+let rgamma shape rate = gamma shape rate |> rdist
+let qgamma shape rate = gamma shape rate |> qdist
+
+// Cauchy Distribution
+let cauchy location scale= Continuous (Cauchy (location, scale))
+let rcauchy location scale = cauchy location scale |> rdist
+let qcauchy location scale = cauchy location scale |> qdist
 
 open System
 let xs = rnorm 1.0 10.0 10000
