aa226e67d3a904689192d1f65892751278398ca9
Anthony Khong <anthony.kusumo.khong@gmail.com>
2019-01-07 19:01:16 +0700

    Started with Clojure solutions

--- clojure/1-introduction-to-simulation-methods.clj
+++ clojure/1-introduction-to-simulation-methods.clj
@@ -1,39 +1,19 @@
-(use '(incanter core stats charts io))
-
-; Utility Functions
-(defn standardise [xs]
-  (let [mu (mean xs)
-        sigma (sd xs)]
-    (->> xs
-         (map #(- % mu))
-         (map #(/ % sigma)))))
-
-(defn quantile-map [xs probs]
-  (zipmap probs (quantile xs :probs probs)))
-
-(defn summarise [xs]
-  (let [std-xs (standardise xs)]
-    {:std-quantiles (quantile-map std-xs [0.025, 0.15, 0.5, 0.85, 0.975])
-     :mean (mean xs)
-     :var (variance xs)
-     :sd (sd xs)
-     :kurtosis (kurtosis xs)
-     :skewness (skewness xs)}))
+(ns intro-to-sim-methods
+  (:require [clojure.pprint :as pp]
+            [incanter.charts :as charts]
+            [incanter.core :as core]
+            [incanter.distributions :as dists]
+            [incanter.stats :as stats]
+            [tools]))
 
 ; Question 1
 (defn sample-single-average [n-rows]
-  (mean (sample-uniform n-rows)))
+  (stats/mean (stats/sample-uniform n-rows)))
 
 (defn sample-averages [n-sims n-rows]
   (take n-sims (repeatedly #(sample-single-average n-rows))))
 
-(for [i (range 1 14 4)
-      :let [n-rows (int (Math/pow 2 i))]]
-  (-> (sample-averages 5000 n-rows)
-      (summarise)
-      (assoc :n-rows n-rows)
-      (pprint)))
-
+; TODO
 
 (defn expectation-of-average [n-rows] 0.5)
 (defn variance-of-average [n-rows] (/ 1/12 n-rows))
@@ -42,9 +22,7 @@
                              :mean (expectation-of-average n-rows)
                              :var (variance-of-average n-rows)})
 
-(for [i (range 1 15 3)
-      :let [n-rows (int (Math/pow 2 i))]]
-  (pprint (expectations n-rows)))
+; TODO
 
 ; Question 2
 (defn sample-sigle-delta [n-x n-y]
@@ -55,12 +33,7 @@
 (defn sample-deltas [n-sims & {:keys [n-x n-y]}]
   (take n-sims (repeatedly #(sample-sigle-delta n-x n-y))))
 
-(for [i (range 1 14 4)
-      :let [n-rows (int (Math/pow 2 i))]]
-  (-> (sample-deltas 5000 :n-x n-rows :n-y n-rows)
-      (summarise)
-      (assoc :n-rows n-rows)
-      (pprint)))
+; TODO
 
 (defn expectation-of-delta [n-x n-y]
   {:mean (- 1 (* 3 2))
@@ -69,8 +42,4 @@
                 (Math/sqrt (+ var-x var-y)))})
 (expectation-of-delta 100 100)
 
-(for [i (range 1 14 4)
-      :let [n-rows (int (Math/pow 2 i))]]
-  (-> (expectation-of-delta n-rows n-rows)
-      (assoc :n-rows n-rows)
-      (pprint)))
+; TODO
--- clojure/tools.clj
+++ clojure/tools.clj
@@ -1,13 +1,24 @@
 (ns tools)
 
+(defn standardise [xs]
+  (let [mu (mean xs)
+        sigma (sd xs)]
+    (->> xs
+         (map #(- % mu))
+         (map #(/ % sigma)))))
+
+(defn quantile-map [xs probs]
+  (zipmap probs (quantile xs :probs probs)))
+
+(defn summarise [xs]
+  (let [std-xs (standardise xs)]
+    {:std-quantiles (quantile-map std-xs [0.025, 0.15, 0.5, 0.85, 0.975])
+     :mean (mean xs)
+     :var (variance xs)
+     :sd (sd xs)
+     :kurtosis (kurtosis xs)
+     :skewness (skewness xs)}))
+
 (defn linspace [min max size]
-  (let [step (/ (- max min) (- size 1)]
-    (loop [output []
-           rec-min min]
-      (if (< rec-min max)
-        (recur (conj output rec-min) (+ rec-min step))
-        output))))
-
-(count (linspace 0.0 1.0 33))
-
-(pos-int? 1.0)
+  (let [step (/ (- max min) (dec size))]
+    (range min (+ max (/ step 2)) step)))
