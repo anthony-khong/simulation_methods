def105a4b7ac26c43416c6edf38a5ac15771ab52
Anthony Khong <anthony.kusumo.khong@gmail.com>
2019-01-08 12:15:11 +0700

    Finished Q1

--- clojure/1-introduction-to-simulation-methods.clj
+++ clojure/1-introduction-to-simulation-methods.clj
@@ -3,26 +3,33 @@
             [incanter.charts :as charts]
             [incanter.core :as core]
             [incanter.distributions :as dists]
-            [incanter.stats :as stats]
-            [tools]))
+            [incanter.stats :as stats]))
+
+(clojure.main/load-script "tools.clj")
 
 ; Question 1
 (defn sample-single-average [n-rows]
   (stats/mean (stats/sample-uniform n-rows)))
 
 (defn sample-averages [n-sims n-rows]
-  (take n-sims (repeatedly #(sample-single-average n-rows))))
-
-; TODO
+  (repeatedly n-sims #(sample-single-average n-rows)))
 
 (defn expectation-of-average [n-rows] 0.5)
+
 (defn variance-of-average [n-rows] (/ 1/12 n-rows))
 
-(defn expectations [n-rows] {:n-rows n-rows
-                             :mean (expectation-of-average n-rows)
-                             :var (variance-of-average n-rows)})
+(defn pdf-of-average [n-rows xs]
+  (let [mu (expectation-of-average n-rows)
+        sigma (Math/sqrt (variance-of-average n-rows))]
+    (stats/pdf-normal xs :mean mu :sd sigma)))
 
-; TODO
+(for [i (range 0 10 2)
+      :let [n-rows (int (Math/pow 2 i))
+            xs (sample-averages 10000 n-rows)
+            pdf (partial pdf-of-average n-rows)]]
+  (-> (charts/histogram xs :nbins 25 :density true)
+      (charts/add-function pdf 0.0 1.0)
+      (core/save (str "figures/ex1-uniform-n" n-rows ".png"))))
 
 ; Question 2
 (defn sample-sigle-delta [n-x n-y]
--- clojure/tools.clj
+++ clojure/tools.clj
@@ -1,23 +1,27 @@
-(ns tools)
+(ns intro-to-sim-methods
+  (:require [incanter.charts :as charts]
+            [incanter.core :as core]
+            [incanter.distributions :as dists]
+            [incanter.stats :as stats]))
 
 (defn standardise [xs]
-  (let [mu (mean xs)
-        sigma (sd xs)]
+  (let [mu (stats/mean xs)
+        sigma (stats/sd xs)]
     (->> xs
          (map #(- % mu))
          (map #(/ % sigma)))))
 
 (defn quantile-map [xs probs]
-  (zipmap probs (quantile xs :probs probs)))
+  (zipmap probs (stats/quantile xs :probs probs)))
 
 (defn summarise [xs]
   (let [std-xs (standardise xs)]
     {:std-quantiles (quantile-map std-xs [0.025, 0.15, 0.5, 0.85, 0.975])
-     :mean (mean xs)
-     :var (variance xs)
-     :sd (sd xs)
-     :kurtosis (kurtosis xs)
-     :skewness (skewness xs)}))
+     :mean (stats/mean xs)
+     :var (stats/variance xs)
+     :sd (stats/sd xs)
+     :kurtosis (stats/kurtosis xs)
+     :skewness (stats/skewness xs)}))
 
 (defn linspace [min max size]
   (let [step (/ (- max min) (dec size))]
